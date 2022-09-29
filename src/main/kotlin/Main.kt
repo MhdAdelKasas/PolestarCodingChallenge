01dfca723ca222dfb1d3f411b3fd23e8e05411ef
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2022-09-29 20:00:40 +0200

    implement the Illegal score from a files including lines of brakets

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,61 @@
 import java.io.File
 
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+var totalFileScore = 0
+
+val openBrackets = listOf('(', '[', '{', '<')
+val closeBrackets = listOf(')', ']', '}', '>')
+fun main(args: Array<String>) {
     File("src/main/resources/input.txt").forEachLine {
         println(it)
+        calculateLineIllegalScore(it)
     }
+    println("Total file score: $totalFileScore")
+}
+
+fun calculateLineIllegalScore(line: String) {
+    val lineChars = line.toCharArray().toMutableList()
+    for (i in 0 until lineChars.size - 1) {
+        if (openBrackets.contains(lineChars[i]) && closeBrackets.contains(lineChars[i + 1])) {
+            if (!isOpenThenCloseItemValid(lineChars[i], lineChars[i + 1])) {
+                addToIllegalScore(lineChars[i + 1])
+                println("Illegal char: ${lineChars[i + 1]}")
+                println("Illegal score: $totalFileScore")
+            }
+            lineChars.removeAt(i)
+            lineChars.removeAt(i)
+            break
+        }
+    }
+    if (doesContainCloseBracket(lineChars)) {
+        calculateLineIllegalScore(lineChars.joinToString(""))
+    }
+}
+
+fun isOpenThenCloseItemValid(openBracket: Char, closeBracket: Char): Boolean {
+    return when (openBracket) {
+        '(' -> closeBracket == ')'
+        '[' -> closeBracket == ']'
+        '{' -> closeBracket == '}'
+        '<' -> closeBracket == '>'
+        else -> false
+    }
+}
+
+private fun addToIllegalScore(char: Char) {
+    when (char) {
+        ')' -> totalFileScore += 3
+        ']' -> totalFileScore += 57
+        '}' -> totalFileScore += 1197
+        '>' -> totalFileScore += 25137
+    }
+}
+
+fun doesContainCloseBracket(lineChars: MutableList<Char>): Boolean {
+    for (i in 0 until lineChars.size) {
+        if (closeBrackets.contains(lineChars[i])) {
+            return true
+        }
+    }
+    return false
 }
