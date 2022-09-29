dd25a4b46c03faea021bfd72bf7271b0779ec655
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2022-09-30 00:46:48 +0200

    calculate the incomplete lines middle score

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,26 +2,25 @@ import java.io.File
 
 
 var totalFileScore = 0
-val inCompleteLinesScores: MutableList<Int> = mutableListOf()
+var incompleteLinesScores: MutableList<Double> = mutableListOf()
 val openBrackets = listOf('(', '[', '{', '<')
 val closeBrackets = listOf(')', ']', '}', '>')
 fun main(args: Array<String>) {
-    /*File("src/main/resources/input.txt").forEachLine {
+    File("src/main/resources/input.txt").forEachLine {
         val oldFileScore = totalFileScore
         println(it)
         calculateLineIllegalScore(it)
         if (oldFileScore == totalFileScore) {
-            File("src/main/resources/illegalLines.txt").appendText(it + "\n")
+            File("src/main/resources/incompleteLines.txt").appendText(it + "\n")
         }
     }
-    println("Total file score: $totalFileScore")*/
-    File("src/main/resources/exampleInput.txt").forEachLine {
+    println("Total file score: $totalFileScore")
+    File("src/main/resources/incompleteLines.txt").forEachLine {
         println(it)
         calculateInCompleteLinesScore(it)
     }
-    println("inCompleteLinesScores: $inCompleteLinesScores")
-    inCompleteLinesScores.sort()
-    println("inCompleteLinesScores: $inCompleteLinesScores")
+    incompleteLinesScores.sort()
+    println("The Middle Score: ${incompleteLinesScores[incompleteLinesScores.size / 2]}")
 }
 
 fun calculateLineIllegalScore(line: String) {
@@ -30,8 +29,8 @@ fun calculateLineIllegalScore(line: String) {
         if (openBrackets.contains(lineChars[i]) && closeBrackets.contains(lineChars[i + 1])) {
             if (!isOpenThenCloseItemValid(lineChars[i], lineChars[i + 1])) {
                 addToIllegalScore(lineChars[i + 1])
-                println("Illegal char: ${lineChars[i + 1]}")
-                println("Illegal score: $totalFileScore")
+                //println("Illegal char: ${lineChars[i + 1]}")
+                //println("Illegal score: $totalFileScore")
             }
             lineChars.removeAt(i)
             lineChars.removeAt(i)
@@ -72,14 +71,15 @@ fun doesContainCloseBracket(lineChars: MutableList<Char>): Boolean {
 }
 
 fun calculateInCompleteLinesScore(line: String) {
-    val lineWithRemovedRoundBrackets = countAndRemoveBrackets(line, '(', ')')
-    val lineWithRemovedSquareBrackets = countAndRemoveBrackets(lineWithRemovedRoundBrackets, '[', ']')
-    val lineWithRemovedCurlyBrackets = countAndRemoveBrackets(lineWithRemovedSquareBrackets, '{', '}')
-    val lineWithRemovedAngleBrackets = countAndRemoveBrackets(lineWithRemovedCurlyBrackets, '<', '>')
+    val lineWithRemovedRoundBrackets = removeSelectedBrackets(line, '(', ')')
+    val lineWithRemovedSquareBrackets = removeSelectedBrackets(lineWithRemovedRoundBrackets, '[', ']')
+    val lineWithRemovedCurlyBrackets = removeSelectedBrackets(lineWithRemovedSquareBrackets, '{', '}')
+    val lineWithRemovedAngleBrackets = removeSelectedBrackets(lineWithRemovedCurlyBrackets, '<', '>')
+
+    println(lineWithRemovedAngleBrackets)
 
     val lineChar = lineWithRemovedAngleBrackets.toCharArray().toMutableList()
-    println(lineWithRemovedAngleBrackets)
-    var incompleteLineScore = 0
+    var incompleteLineScore: Double = 0.0
     for (i in  lineChar.size - 1 downTo 0) {
         when (lineChar[i]) {
             '(' -> incompleteLineScore = (incompleteLineScore * 5) + 1
@@ -88,28 +88,29 @@ fun calculateInCompleteLinesScore(line: String) {
             '<' -> incompleteLineScore = (incompleteLineScore * 5) + 4
         }
     }
-    inCompleteLinesScores.add(incompleteLineScore)
+    incompleteLinesScores.add(incompleteLineScore)
 }
 
-fun countAndRemoveBrackets(line: String, openBracket: Char, closeBracket: Char): String {
+fun removeSelectedBrackets(line: String, openBracket: Char, closeBracket: Char): String {
     val lineChar = line.toCharArray().toMutableList()
-    val openBracketsCount = line.count { it == openBracket }
-    val closeBracketsCount = line.count { it == closeBracket }
-    if (openBracketsCount == closeBracketsCount) {
-        lineChar.removeAll(listOf('<'))
-        lineChar.removeAll(listOf('>'))
-    } else {
-        if (openBracketsCount > 1) {
-            for (i in 0 until closeBracketsCount) {
-                if (lineChar.indexOfLast { it == closeBracket } > lineChar.indexOfLast { it == openBracket }) {
-                    lineChar.removeAt(lineChar.indexOfLast { it == openBracket })
-                    lineChar.removeAt(lineChar.indexOfLast { it == closeBracket })
-                } else {
-                    lineChar.removeAt(lineChar.indexOfFirst { it == closeBracket })
-                    lineChar.removeAt(lineChar.indexOfFirst { it == openBracket })
+    val indicesToRemove = mutableListOf<Int>()
+    val openBracketsIndices = line.indices.filter { lineChar[it] == openBracket }.toMutableList()
+    val closeBracketsIndices = line.indices.filter { lineChar[it] == closeBracket }.toMutableList()
+    for (close in closeBracketsIndices) {
+        for (i in openBracketsIndices.size - 1 downTo 0) {
+            if (openBracketsIndices[i] < close) {
+                if (!indicesToRemove.contains(openBracketsIndices[i])) {
+                    indicesToRemove.add(openBracketsIndices[i])
+                    openBracketsIndices.removeAt(i)
                 }
+                if (!indicesToRemove.contains(close)) indicesToRemove.add(close)
+                break
             }
         }
+    }
+    indicesToRemove.sortDescending()
+    for (index in indicesToRemove) {
+        lineChar.removeAt(index)
     }
     return lineChar.joinToString("")
 }
